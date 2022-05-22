d4b15ac551270722fba071151c1d737ff18857f6
Naths <ndimagiba011117@gmail.com>
2022-05-22 15:29:40 +0800

    update

--- myscript.js
+++ myscript.js
@@ -7,4 +7,30 @@ document.getElementById("button").addEventListener("click", function() {
     var song = document.getElementById("song")
     song.volume = 1
     song.play()
-})
+});
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
+
+document.onkeydown = function (e){
+    if(event.keyCode == 123){
+        return false;
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+        return false;
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+        return false;
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+        return false;
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.keyCode == "U".charCodeAt(0)) {
+        return false;
+    }
+};
